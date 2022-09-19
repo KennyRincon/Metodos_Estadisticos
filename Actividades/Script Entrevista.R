dbe70066023d39fb8d82e7f045c08a4a4735c44b
Kenny Rincon <Kenny_Margarita_Lopez@hotmail.com>
2022-09-19 09:06:31 -0500

    update

--- Actividades/Script Entrevista.R
+++ Actividades/Script Entrevista.R
@@ -33,31 +33,72 @@ round(gen/length(Enc1$Genero)*100,1)
 
 
 
-#¿Que equipo entrevistador tuvo mas encuestas?
+# ¿Que equipo entrevistador tuvo mas encuestas?
 table(Enc1$Entrev)
 prop.table(ent)*100 
 pie(prop.table(ent)*100)
 
-#¿Cual es el porcentaje de alumnos entrevistados por carrera?
+# ¿Cual es el porcentaje de alumnos entrevistados por carrera?
 car <- table(Enc1$Carrera)
 prop.table(car)*100
 
-#¿Cuantos alumnos participaron por semestre?
+# ¿Cuantos alumnos participaron por semestre?
 sem <- table(sem)*100
 
-#¿Cual es el rango de edad de los participantes?
+# ¿Cual es el rango de edad de los participantes?
 range(Enc1$Edad)
 
-#¿Como te enteraste de la facultad?
+# ¿Como te enteraste de la facultad?
 conFCF <- table(Enc1$oi_1)
 prop.table(conFCF)*100
 pie(prop.table(conFCF)*100)
 
-
-#¿Fue tu primera opcion la FCF?
+ 
+# ¿Fue tu primera opcion la FCF?
 op <- table(Enc1$oi_2)
-prop.table(of)*100
+prop.table(op)*100
 
-#¿Presentaste en otra facultad?
+# ¿Presentaste en otra facultad?
 of <- table(Enc1$oi_2a)
 prop.table(of)*100
+
+# Estuviste inscrio en otra facultad?
+io_4a <- table(Enc1$oi_4a)
+prop.table(oi_4a)*100
+oi_4a
+
+#Convivencia
+ef_1 <- table(Enc1$ef_1)
+c.alum
+prop.table(c.alum)*100
+pie(prop.table(c.alum)*100)
+
+#Instalaciones de la facultad
+insf <- table(Entrevista$ins_1)
+insf
+prop.table(insf)*100
+pie(prop.table(insf)*100)
+
+#Conoces la malla curricular de tu carrera?
+mcu <- table(Entrevista$ap_1)
+mcu
+prop.table(mcu)*100
+pie(prop.table(mcu)*100)
+
+#Entregas tareas a tiempo?
+Tarea <- table(Entrevista$ap_2)
+pie(prop.table(insf)*100)
+tarea
+prop.table(tarea)*100
+pie(prop.table(tarea)*100)
+
+#Estas concentrado durante clase?
+clase <- table(entrevista$ap_3)
+clase
+prop.table(clase)*100
+pie(prop.table(clase)*100)
+
+
+
+#Guardar la BD con los datos nuevos en formato csv
+write.csv(Enc1, "Encuesta_codificada.csv")
