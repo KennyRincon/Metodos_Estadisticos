3845bb432d8292cc2d3b426b760cfc8bd49f98b9
Kenny Rincon <Kenny_Margarita_Lopez@hotmail.com>
2022-09-28 08:28:12 -0500

    Borrar

--- Actividades/Entrevista.R
+++
@@ -1,7 +0,0 @@
-Entrevista <- read.csv("Datos/Encuesta_codificada.csv",header = T)
-
-Entrevista$Entrev <- as.factor(Entrevista$Entrev)
-Entrevista$oi_2a <- as.factor(Entrevista$oi_2a)
-Entrevista$oi_4a <- as.factor(Entrevista$oi_4a)
-Entrevista$Carrera <- as.factor(Entrevista$Carrera)
-
