4b9cb5ca8eedd419de67057802c820613b3f2d77
Kenny Rincon <Kenny_Margarita_Lopez@hotmail.com>
2022-09-28 08:18:40 -0500

    Update

--- Actividades/Script Entrevista.R
+++ Actividades/Script Entrevista.R
@@ -1,104 +1,120 @@
-#KMRL
+# KENNY
+# ENTREVISTA
 
-#Importar encuesta
+Entrevista <- read.csv("Datos/Encuesta_codificada.csv", header = T)
 
-Enc1 <- read.csv("Datos/Encuesta_codificada.csv", header = T)
+Entrevista$Entrev <- as.factor(Entrevista$Entrev)
+levels(Entrevista$Entrev)
 
-# convertir variables a factores
+Entrevista$Carrera <- as.factor(Entrevista$Carrera)
+levels(Entrevista$Carrera)
 
-Enc1$Entrev <- as.factor(Enc1$Entrev)
-levels(Enc1$Entrev)
+Entrevista$Genero <- as.factor(Entrevista$Genero)
+levels(Entrevista$Genero)
 
-
-Enc1$Genero <- as.factor(Enc1$Genero)
-levels(Enc1$Genero) 
-
-Enc1$Carrera <- as.factor(Enc1$Carrera)
-levels(Enc1$Carrera)
-
-Enc1$Semestre <- as.factor(Enc1$Semestre)
-levels(Enc1$Semestre)
-
-
-
-#Funcion para transformar caracteres a fechas 
+# Función para transformar carácteres a fechas
 
 library(lubridate)
-# Enc1$Fecha <- as_factor(Enc1$Fecha) #Pendiente de arreglar
+Entrevista$Fecha <- as.Date(Entrevista$Fecha)
 
-#¿cual es el porcentaje de entrevistados por genero?
+# ¿Cuál fu el porcentaje de entrevistados por género?
+gen <- table(Entrevista$Genero)
+round(gen/length(Entrevista$Genero)*100,1)
+pie(round(gen/length(Entrevista$Genero)*100,1))
 
-gen <- table(Enc1$Genero)
-round(gen/length(Enc1$Genero)*100,1)
-
-
-
-# ¿Que equipo entrevistador tuvo mas encuestas?
-table(Enc1$Entrev)
-prop.table(ent)*100 
+# ¿Qué equipo entrevistador tuvo más encuestas?
+ent <- table(Entrevista$Entrev)
+prop.table(ent)*100
 pie(prop.table(ent)*100)
 
-# ¿Cual es el porcentaje de alumnos entrevistados por carrera?
-car <- table(Enc1$Carrera)
+# ¿Cuál es el porcentaje de alumnos entrevistados por carrera?
+car <- table(Entrevista$Carrera)
 prop.table(car)*100
+pie(prop.table(car)*100)
 
-# ¿Cuantos alumnos participaron por semestre?
-sem <- table(sem)*100
+# ¿Cuántos alumnos participaron por semetre?
+sem <- table(Entrevista$Semestre)
+prop.table(sem)*100
+pie(prop.table(sem)*100)
 
-# ¿Cual es el rango de edad de los participantes?
-range(Enc1$Edad)
+# ¿Cuál es el rango de edad de los participantes?
+range(Entrevista$Edad)
 
-# ¿Como te enteraste de la facultad?
-conFCF <- table(Enc1$oi_1)
+# ¿Cómo te enteraste de la facultad?
+conFCF <- table(Entrevista$oi_1)
+conFCF
 prop.table(conFCF)*100
-pie(prop.table(conFCF)*100)
+pie(prop.table(conFCF)*100,
+    col=c("red", "orange", "pink", "blue", "green"))
+# col=palette("buscar en internet") / paleta <- c("colores")
 
- 
-# ¿Fue tu primera opcion la FCF?
-op <- table(Enc1$oi_2)
+# ¿Fue tu promera opción la FCF?
+op <- table(Entrevista$oi_2)
+op
 prop.table(op)*100
+pie(prop.table(op)*100)
+
+# ¿Presentantes en otra facultad?
+of <- table(Entrevista$oi_2a)
+of
+prop.table(of)*100
+pie(prop.table(of)*100)
+
+# ¿Fue tu promera opción la FCF?
+op <- table(Entrevista$oi_2)
+op
+prop.table(op)*100
+pie(prop.table(op)*100)
 
 # ¿Presentaste en otra facultad?
-of <- table(Enc1$oi_2a)
+of <- table(Entrevista$oi_2a)
+of
 prop.table(of)*100
+pie(prop.table(of)*100)
 
-# Estuviste inscrio en otra facultad?
-io_4a <- table(Enc1$oi_4a)
-prop.table(oi_4a)*100
-oi_4a
+# ¿Estuviste inscrito en otra facultad?
+fac <- table(Entrevista$oi_4)
+fac
+prop.table(fac)*100
+pie(prop.table(fac)*100)
 
-#Convivencia
-ef_1 <- table(Enc1$ef_1)
+# ¿Dónde estaba inscrito?
+fa <- table(Entrevista$oi_4a)
+fa
+prop.table(fa)*100
+pie(prop.table(fa)*100)
+
+# Convivencia entre alumnos
+c.alum <- table(Entrevista$ef_1)
 c.alum
 prop.table(c.alum)*100
 pie(prop.table(c.alum)*100)
 
-#Instalaciones de la facultad
+# Instalaciones de la facultad
 insf <- table(Entrevista$ins_1)
 insf
 prop.table(insf)*100
 pie(prop.table(insf)*100)
 
-#Conoces la malla curricular de tu carrera?
+# ¿Conoces la malla curricular de tu carrera?
 mcu <- table(Entrevista$ap_1)
 mcu
 prop.table(mcu)*100
 pie(prop.table(mcu)*100)
 
-#Entregas tareas a tiempo?
-Tarea <- table(Entrevista$ap_2)
-pie(prop.table(insf)*100)
+# ¿Entregas tareas a tiempo?
+tarea <- table(Entrevista$ap_2)
 tarea
 prop.table(tarea)*100
 pie(prop.table(tarea)*100)
 
-#Estas concentrado durante clase?
-clase <- table(entrevista$ap_3)
+# ¿Estás concentrado durante las clases?
+clase <- table(Entrevista$ap_3)
 clase
 prop.table(clase)*100
 pie(prop.table(clase)*100)
 
 
+# GUARDAR LA BD CON LOS DATOS NUEVOS EN FORMATO.CSV
+write.csv(Entrevista, "Datos/Encuesta_codificada.csv")
 
-#Guardar la BD con los datos nuevos en formato csv
-write.csv(Enc1, "Encuesta_codificada.csv")
